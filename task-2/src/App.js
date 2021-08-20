3c58773a892509f3952025e92fb1d08f57d5613e
akashsaravanan007 <58935531+akashsaravanan007@users.noreply.github.com>
2021-08-20 12:38:49 +0530

    first commit

---
+++ task-2/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import "./index.css";
+
+const App = () => {
+  const [users, setUser] = useState([]);
+  const loadUsers = async () => {
+   
+    const response = await fetch("https://api.github.com/users");
+    const jsonresponse = await response.json();
+    setUser(jsonresponse);
+  };
+
+  return (
+    <div className="App">
+      <h1>ZIPPY Users</h1>
+      <h2>Hello All, Click Get users button to get user and their details</h2>
+      <button className="btn-user" onClick={loadUsers}>Get Users</button>
+      <h2>Users will appear below:</h2>
+      
+      
+        <div className = "user-row">
+        {users.map(({ id, login, avatar_url }) => {
+          
+                      return (
+                        
+              <div className="user-card user-column" key={id}>
+                <img src={avatar_url} className="uppercard" alt="..." />
+                <div className="card-body">
+                  <h3 className="title-card-user">
+                    {login}
+                  </h3>
+                  <h4 className="user-text"></h4>
+                </div>
+              </div>
+            );
+            
+                      })}
+      </div>
+      </div>
+    
+  );
+};
+export default App;
